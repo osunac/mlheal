a61116a3a2081a9296d0f609fd01b0a22a73716e
Christophe Osuna <christophe@redmintnetwork.fr>
2019-01-02 21:51:28 +0100

    Generate points for last days

--- mlheal/app/main.go
+++ mlheal/app/main.go
@@ -30,7 +30,41 @@ func breakApp(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Application is broken now.\n")
 }
 
+func pastDates(start, stop time.Time, step time.Duration) chan time.Time {
+	c := make(chan time.Time)
+	go func() {
+		for t := start; t.Before(stop); t = t.Add(step) {
+			c <- t
+		}
+		close(c)
+	}()
+
+	return c
+}
+
 func main() {
+	stop := time.Now()
+	start := stop.Add(-3 * 24 * time.Hour)
+	step := 24 * time.Hour
+	duration := 100000
+
+	for t := range pastDates(start, stop, step) {
+		log.Printf("HTTP - %s - - %s \"%s %s %s\" %d %d \"%s\" \"%s\" %dus\n",
+			"127.0.0.1",
+			t.Format("[02/Jan/2006:15:04:05 -0700]"),
+			//"[02/Jan/2006:15:04:05 -0700]",
+			"GET",
+			"/",
+			"HTTP/1.1",
+			200,
+			13,
+			"",
+			"App Client",
+			//time.Since(t)/1000,
+			duration,
+		)
+	}
+
 	serveMux := http.NewServeMux()
 
 	serveMux.HandleFunc("/", hello)
